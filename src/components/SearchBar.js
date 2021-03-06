5e015280b7ac050d991dc8e9dcec5f07bb7d5f69
Denis <denis.tojaga@edu.fit.ba>
2021-03-06 17:12:17 +0100

    //maganing State

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,6 @@
 import React from "react";
-import {View,Text,TouchableOpacity,StyleSheet} from "react-native";
-import {Feather} from "@expo/vector-icons";
-import { TextInput } from "react-native-gesture-handler";
+import { View, Text, StyleSheet, TextInput } from "react-native";
+import { Feather } from "@expo/vector-icons";
 
 //name of the icon from the whole directory like a component
 
@@ -15,13 +14,30 @@ import { TextInput } from "react-native-gesture-handler";
 //We show it exactly like a custom component <Feather />
 //For icon styling we can make a styling object or assign a prop directly to it ( size and color )
 
-const SearchBar = ()=>{
+const SearchBar = ({ term, onTermChange }) => {
+
+
+
+    //the value by default will be the term we got as a first argument and on changing text we call a function "onChangeTerm"
+    //that function need some new text in order to set the new term so we send that as well as an argument
+
 
     return (
-        <View style = {styles.backgroundStyle}>
+        <View style={styles.backgroundStyle}>
 
-            <Feather name = "search" style={styles.iconStyle}/>
-            <TextInput style = {styles.searchText} placeholder="Search for restaurants"></TextInput>
+            <Feather name="search" style={styles.iconStyle} />
+
+            <TextInput
+                style={styles.searchText}
+                placeholder="Search for restaurants"
+                value={term}
+                autoCorrect = {false}
+                autoCapitalize = "sentences"
+                onChangeText={(newValue) => {
+                    onTermChange(newValue)
+                }}
+
+            ></TextInput>
 
         </View>
     );
@@ -34,31 +50,31 @@ const SearchBar = ()=>{
 
 const styles = StyleSheet.create({
 
-    backgroundStyle:{
-        backgroundColor:"lightgray",
-        marginHorizontal:15,        
-        marginVertical:15,
-        borderRadius:5,
-        padding:2,
-        height:50.5,
-        flexDirection:"row"
+    backgroundStyle: {
+        backgroundColor: "lightgray",
+        marginHorizontal: 15,
+        marginVertical: 15,
+        borderRadius: 5,
+        padding: 2,
+        height: 50.5,
+        flexDirection: "row"
     },
 
 
-    iconStyle:{
-        color:"black",
-        fontSize:39,
-        alignSelf:"center",
-        marginHorizontal:10       
+    iconStyle: {
+        color: "black",
+        fontSize: 39,
+        alignSelf: "center",
+        marginHorizontal: 10
     },
 
-    searchText:{
-        flex:1,
-        fontSize:21
+    searchText: {
+        flex: 1,
+        fontSize: 18
     }
 
 });
- 
+
 
 
 export default SearchBar;
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React,{useState} from "react";
 import {View,Text,StyleSheet} from "react-native";
 import SearchBar from "../components/SearchBar";
 
 
 const SearchScreen = ()=>{
+
+    //term will be the value we are searching for in a searchBar component
+    const [term,setTerm] = useState("");
+
+
+    //passing two props to a searchBar component
+    //first we pass in the term that is currently displayed, then we pass the function onTermChange
+    //in that function we need to accept one argument and thats a new term and the function is setting the newTerm as term
+
     return (
         <View>
-            <SearchBar />
+            <SearchBar term = {term} onTermChange = {(newTerm)=>{setTerm(newTerm)}} />
             <Text>Search Screen</Text>
         </View>
     );
