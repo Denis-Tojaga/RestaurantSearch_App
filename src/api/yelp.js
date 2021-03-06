10a3cd84efd3c12dcc7fa0e070d451adc936002a
Denis <denis.tojaga@edu.fit.ba>
2021-03-06 18:45:34 +0100

    //errorCatching

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -5,6 +5,7 @@ export default axios.create({
     //bez / na kraju
     //ova instanca ce uvijek imate baseURL nakacen na request mi cemo samo dodavati ono sto specificno trazimo
     //yelp.get("/search")
+
     baseURL: "https://api.yelp.com/v3/businesses",
     //druga stvar je da uspostavimo autentifikaciju sa API a to radimo kroz headers objekat koji zahtjeva API key, sintaksa je Bearer + space + API key
     headers:{
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
+//importujemo yelp file iz API foldera
 import yelp from "../api/yelp";
 
 
@@ -20,12 +21,11 @@ import yelp from "../api/yelp";
 
 const SearchScreen = () => {
 
-    //term will be the value we are searching for in a searchBar component
+    //searchInput will be the value we are searching for in a searchBar component
     const [searchInput, setSearchInput] = useState("");
 
-
     //passing two props to a searchBar component
-    //first we pass in the term that is currently displayed, then we pass the function onTermChange
+    //first we pass in the term that is currently displayed, then we pass the function onInputChange
     //in that function we need to accept one argument and thats a new term and the function is setting the newTerm as term
 
 
@@ -35,20 +35,37 @@ const SearchScreen = () => {
     const [restaurants, setRestaurants] = useState([]);
 
 
-    const searchAPI = async () =>
-     {
 
-        var APIresponse = await yelp.get("/search", {
-            params: {
-                limit: 50,
-                term: searchInput,
-                location: "San Jose"
-            }
-        });
+    //ukoliko dodje do errora onda moramo updateovati screen da se prikaze errorMessage
+    const [errorMessage, setErrorMessage] = useState("");
 
-        //nama iz tog objekta treba samo objekat businesses 
-        setRestaurants(APIresponse.data.businesses);
+
+
+
+
+
+    const searchAPI = async () => {
+
+        try {
+            var APIresponse = await yelp.get("/search", {
+                params: {
+                    limit: 50,
+                    term: searchInput,
+                    location: "San Jose"
+                }
+            });
+
+            //nama iz tog objekta treba samo objekat businesses 
+            setRestaurants(APIresponse.data.businesses);
+
+        } catch (err) {
+            setErrorMessage("Something went wrong!")
+        }
+
     };
+
+
+
 
 
 
@@ -57,13 +74,17 @@ const SearchScreen = () => {
         <View>
 
             <SearchBar
+                //postavlja searchInput
                 searchInput={searchInput}
+                //svaki put kad se promijeni input da se postavi na novu vrijednost
                 onInputChange={(newInput) => setSearchInput(newInput)}
                 //svaki put na pretrazivanje pozivamo asinhronu helper funkciju
                 onSearch={() => searchAPI()}
             />
 
-            <Text>Search Screen</Text>
+
+            {errorMessage.length != 0 ? <Text>{errorMessage}</Text> : null}
+
             <Text>We have found {restaurants.length}</Text>
 
         </View>
