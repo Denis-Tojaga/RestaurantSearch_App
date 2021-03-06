8647488490940c5939401298d1397d43bfd0d8f7
Denis <denis.tojaga@edu.fit.ba>
2021-03-06 09:52:40 +0100

    //module instalation

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {createAppContainer} from "react-navigation";
+import {createStackNavigator} from "react-navigation-stack";
+
 
 export default function App() {
   return (
