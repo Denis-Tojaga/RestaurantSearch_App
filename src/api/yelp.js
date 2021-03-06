137d33ec7bb4c55ef33677d6f49af629922811e1
Denis <denis.tojaga@edu.fit.ba>
2021-03-06 17:39:34 +0100

    //yelp finished

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -4,6 +4,10 @@ export default axios.create({
 
     //bez / na kraju
     //ova instanca ce uvijek imate baseURL nakacen na request mi cemo samo dodavati ono sto specificno trazimo
-    baseURL: "https://api.yelp.com/v3/businesses"
-
+    //yelp.get("/search")
+    baseURL: "https://api.yelp.com/v3/businesses",
+    //druga stvar je da uspostavimo autentifikaciju sa API a to radimo kroz headers objekat koji zahtjeva API key, sintaksa je Bearer + space + API key
+    headers:{
+        Authorization: "Bearer 7SWYQ0eiT20dCymd3Mr4gtsgybt_3RLvOOps2FC4dXWXE24tfy01vqs_XW3dgLngEk8CkiTSwRlY6AS6qSwZx-xVBHMgQj8H2W1tBNwhhrMEqU5yu8a__7nMUmVCYHYx"
+    }
 });
