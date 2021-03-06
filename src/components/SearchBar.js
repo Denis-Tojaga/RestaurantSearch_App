dfcae7c7bb56764ccdf5b1f45b99688b1c286f25
Denis <denis.tojaga@edu.fit.ba>
2021-03-06 10:51:20 +0100

    //importing icons

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,23 @@
 import React from "react";
 import {View,Text,TouchableOpacity,StyleSheet} from "react-native";
+import {Feather} from "@expo/vector-icons";
+
+//name of the icon from the whole directory like a component
 
 
 
 
 //SearchBar component
 
+
+//Adding icons is easier because we already have vector-icons
+
 const SearchBar = ()=>{
 
     return (
         <View style = {styles.background}>
+
+            <Feather name = "search" />
             <Text>Search Bar</Text>
 
         </View>
@@ -24,8 +32,11 @@ const SearchBar = ()=>{
 const styles = StyleSheet.create({
 
     background:{
-        backgroundColor:"#F0EEEE"
-
+        backgroundColor:"lightgray",
+        height:50.5,
+        marginHorizontal:15,        
+        borderRadius:5,
+        marginVertical:10
     }
 
 });
