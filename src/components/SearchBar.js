9175c6d370255d993092fe0cfa25fb771fbeb4ac
Denis <denis.tojaga@edu.fit.ba>
2021-03-06 10:59:14 +0100

    //textInput styling

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {View,Text,TouchableOpacity,StyleSheet} from "react-native";
 import {Feather} from "@expo/vector-icons";
+import { TextInput } from "react-native-gesture-handler";
 
 //name of the icon from the whole directory like a component
 
@@ -17,8 +18,8 @@ const SearchBar = ()=>{
     return (
         <View style = {styles.background}>
 
-            <Feather name = "search" />
-            <Text>Search Bar</Text>
+            <Feather name = "search" size = {39} color={"black"}/>
+            <TextInput style = {styles.searchText} placeholder="Search for restaurants"></TextInput>
 
         </View>
     );
@@ -36,11 +37,21 @@ const styles = StyleSheet.create({
         height:50.5,
         marginHorizontal:15,        
         borderRadius:5,
-        marginVertical:10
+        marginVertical:10,
+        flexDirection:"row",
+        padding:5
+    },
+
+
+    searchText:{
+        position:"absolute",
+        alignSelf:"center",
+        marginLeft:55,
+        fontSize:22
     }
 
 });
-
+ 
 
 
 export default SearchBar;
